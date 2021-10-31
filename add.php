583399fddfc364b710d3f2118621ffdd91706c4c
ilhamjaya24 <kusumahilhamjaya27@gmail.com>
2021-10-31 16:22:56 +0700

    Update UTS-Ilham

--- add.php
+++ add.php
@@ -33,7 +33,7 @@ if ($dataisi['jenis_buku'] == 1) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>UTS-Ilham Jaya Kusumah-MI20B</title>
 
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="assets/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
